a6e6b36d4ee6e7a894d2eac4e85b91295df6a0d4
Vegard Strand <vegstrVG@gmail.com>
2018-04-26 12:43:10 +0200

    Set Up template 10 Hamburger menu

---
+++ ImageSortLibrary.UWP/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ImageSortLibrary.UWP.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace ImageSortLibrary.UWP.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
---
+++ ImageSortLibrary.UWP/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ImageSortLibrary.UWP.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace ImageSortLibrary.UWP.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
