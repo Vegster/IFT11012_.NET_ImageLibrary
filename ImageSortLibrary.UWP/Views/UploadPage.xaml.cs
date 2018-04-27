b573e815c0259c2541959b6096f0b5b151ddd76f
Vegard Strand <vegstrVG@gmail.com>
2018-04-27 13:09:56 +0200

    Uplaod page and editpage

---
+++ ImageSortLibrary.UWP/Views/UploadPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ImageSortLibrary.UWP.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UploadPage : Page
+    {
+        public UploadPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void OpenFileExplorer(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker
+            {
+                ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary
+            };
+
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if(file != null)
+            {
+                SourceFile.Text = "Picked photo: " + file.Name;
+            }
+            else
+            {
+                SourceFile.Text = "Operation cancelled";
+            }
+
+        }
+
+        
+    }
+}
